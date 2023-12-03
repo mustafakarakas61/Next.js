f7f0d976fdc1aa7ec6cfd6253396f55e79f79231
Mustafa karakaş <mustafakarakas61ts862@gmail.com>
2023-12-03 13:20:48 +0300

    logout up

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -6,7 +6,7 @@ export async function GET() {
             message: "Logout successful",
             success: true,
         })
-        response.cookies.set("token", "", {httpOnly: true, expires: new Date(0), domain: `${process.env.DOMAIN}`});
+        response.cookies.set("token", "", {httpOnly: true, expires: new Date(0),});
         return response;
     } catch (error: any) {
         return NextResponse.json({error: error.message}, {status: 500})
