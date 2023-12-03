0dfeec251e7e5bf6d49b32fb0b96d82d2aab932c
Mustafa karakaş <mustafakarakas61ts862@gmail.com>
2023-12-03 13:32:35 +0300

    logout up

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -1,12 +1,16 @@
 import {NextResponse} from "next/server";
+import { cookies} from "next/headers";
 
 export async function GET() {
     try {
+        const cookieStore = cookies();
+        cookieStore.delete('token');
+
         const response = NextResponse.json({
             message: "Logout successful",
             success: true,
         })
-        response.cookies.set("token", "", {httpOnly: true, expires: new Date(0), path: "/"});
+        // response.cookies.set("token", "", {httpOnly: true, expires: new Date(0), path: "/"});
         return response;
     } catch (error: any) {
         return NextResponse.json({error: error.message}, {status: 500})
